c2076bb0f9a741b1f17b6683c45dff7b52b4c5a0
RSBColab <30296630+RSBColab@users.noreply.github.com>
2017-07-19 18:42:50 +0200

    Fixing lame bugs

--- WorkState.cs
+++ WorkState.cs
@@ -26,16 +26,19 @@ namespace SimpleESP
                 var otherPlayers = Players.RemotePlayers;
                 foreach (var otherPlayer in otherPlayers)
                 {
-                    if (otherPlayer.IsPvpEnabled)
-                        g.SetColor(Color.Orange);
-                    else
-                        g.SetColor(Color.Green);
+                    if (otherPlayer?.ScreenLocation != null)
+                    {
+                        if (otherPlayer.IsPvpEnabled)
+                            g.SetColor(Color.Orange);
+                        else
+                            g.SetColor(Color.Green);
 
 
-                    g.DrawLine(Convert.ToInt32(Players.LocalPlayer.ScreenLocation.X),
-                        Convert.ToInt32(Players.LocalPlayer.ScreenLocation.Y),
-                        Convert.ToInt32(otherPlayer.ScreenLocation.X),
-                        Convert.ToInt32(otherPlayer.ScreenLocation.Y));                    
+                        g.DrawLine(Convert.ToInt32(Players.LocalPlayer.ScreenLocation.X),
+                            Convert.ToInt32(Players.LocalPlayer.ScreenLocation.Y),
+                            Convert.ToInt32(otherPlayer.ScreenLocation.X),
+                            Convert.ToInt32(otherPlayer.ScreenLocation.Y));
+                    }
                 }
             }
 
@@ -44,26 +47,36 @@ namespace SimpleESP
                 var harvestables = Objects.AllHarvestables.FindAll(x => !x.Depleted);
                 var harvestablesValid = new List<IHarvestableObject>();
                 foreach (var harvestable in harvestables)
-                {                    
-                    if ((_configuration.ResFiber && harvestable.Type == Ennui.Api.Meta.ResourceType.Fiber) ||
-                        (_configuration.ResOre && harvestable.Type == Ennui.Api.Meta.ResourceType.Ore) ||
-                        (_configuration.ResStone && harvestable.Type == Ennui.Api.Meta.ResourceType.Rock) ||
-                        (_configuration.ResTree && harvestable.Type == Ennui.Api.Meta.ResourceType.Wood))
+                {
+                    if (harvestable != null)
                     {
-                        if ((harvestable.Tier == 2 && _configuration.ResT2) ||
-                           (harvestable.Tier == 3 && _configuration.ResT3) ||
-                           (harvestable.Tier == 4 && _configuration.ResT4))
+                        if ((_configuration.ResFiber && harvestable.Type == Ennui.Api.Meta.ResourceType.Fiber) ||
+                            (_configuration.ResOre && harvestable.Type == Ennui.Api.Meta.ResourceType.Ore) ||
+                            (_configuration.ResStone && harvestable.Type == Ennui.Api.Meta.ResourceType.Rock) ||
+                            (_configuration.ResTree && harvestable.Type == Ennui.Api.Meta.ResourceType.Wood))
+                        {
+                            if ((harvestable.Tier == 2 && _configuration.ResT2) ||
+                               (harvestable.Tier == 3 && _configuration.ResT3) ||
+                               (harvestable.Tier == 4 && _configuration.ResT4) ||
+                               (harvestable.Tier == 5 && _configuration.ResT5) ||
+                               (harvestable.Tier == 6 && _configuration.ResT6) ||
+                               (harvestable.Tier == 7 && _configuration.ResT7) ||
+                               (harvestable.Tier == 8 && _configuration.ResT8))
                             harvestablesValid.Add(harvestable);
+                        }
                     }
                 }
 
                 foreach (var harvestable in harvestablesValid)
                 {
-                    g.SetColor(Color.Blue);
-                    g.DrawLine(Convert.ToInt32(Players.LocalPlayer.ScreenLocation.X),
-                        Convert.ToInt32(Players.LocalPlayer.ScreenLocation.Y),
-                        Convert.ToInt32(harvestable.ScreenLocation.X),
-                        Convert.ToInt32(harvestable.ScreenLocation.Y));
+                    if(harvestable.ScreenLocation != null)
+                    {
+                        g.SetColor(Color.Blue);
+                        g.DrawLine(Convert.ToInt32(Players.LocalPlayer.ScreenLocation.X),
+                            Convert.ToInt32(Players.LocalPlayer.ScreenLocation.Y),
+                            Convert.ToInt32(harvestable.ScreenLocation.X),
+                            Convert.ToInt32(harvestable.ScreenLocation.Y));
+                    }
                 }
             }
         }
